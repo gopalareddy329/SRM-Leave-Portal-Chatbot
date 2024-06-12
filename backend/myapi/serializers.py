0a53d36a0b81732f63d9ae8eaf5fce3c66fe3f46
Hrithik2212 <hr6466@srmist.edu.in>
2024-06-12 21:24:19 +0530

    migrate to fastapi backend

--- backend/myapi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Usersdata
-
-
-class UsersDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Usersdata
-        fields='__all__'
